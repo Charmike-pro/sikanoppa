4e7dd262e60c137b2bc3fa2698324665606a5b10
Char Mike <storagewars999@gmail.com>
2020-04-29 13:08:59 +0300

    tein ykkös ja kakkos noppa sivut.

---
+++ js.js
@@ -0,0 +1,33 @@
+function rollDice2(){
+
+
+  var die1 = document.getElementById("die1");
+  var die2 = document.getElementById("die2");
+  var status = document.getElementById("status")
+  var d1 = Math.floor(Math.random() * 6) + 1;
+  var d2 = Math.floor(Math.random() * 6) + 1;
+  var diceTotal = d1 + d2;
+  die1.innerHTML = d1;
+  die2.innerHTML = d2;
+  status.innerHTML = "Heitit "+diceTotal+".";
+  if (d1 == '1'){
+   alert("Pelaaja heitti ykkösen. Vuorojen vaihto.")
+   return window.location.reload(false); 
+  }
+   if (d2 == '1'){
+    alert("Pelaaja heitti ykkösen. Vuorojen vaihto.")
+    return window.location.reload(false); 
+}
+}
+function rollDice(){
+
+
+  var die1 = document.getElementById("die1");
+  var status = document.getElementById("status")
+  var d1 = Math.floor(Math.random() * 6) + 1;
+  die1.innerHTML = d1;
+  if (d1 == '1'){
+    alert("Pelaaja heitti ykkösen. Vuorojen vaihto.")
+    return window.location.reload(false); 
+  }
+}
