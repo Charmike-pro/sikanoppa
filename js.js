a0ddd388b24925a4cd44431ece3aef2f3aa34d5e
Char Mike <storagewars999@gmail.com>
2020-04-30 11:24:41 +0300

    Lisäsin alkeellisen pistelaskurin, sekä tein tuplapisteistä toimivat.

--- js.js
+++ js.js
@@ -1,6 +1,5 @@
 function rollDice2(){
 
-
   var die1 = document.getElementById("die1");
   var die2 = document.getElementById("die2");
   var status = document.getElementById("status")
@@ -9,7 +8,14 @@ function rollDice2(){
   var diceTotal = d1 + d2;
   die1.innerHTML = d1;
   die2.innerHTML = d2;
+    var sum = d1 + d2;
+    var mul = sum * 2;
+    document.getElementById('score').innerHTML = sum;
   status.innerHTML = "Heitit "+diceTotal+".";
+  if (d1 == d2){
+    status.innerHTML += " TUPLAT! Saat tuplapisteet!";
+    document.getElementById('score').innerHTML = mul;
+  }
   if (d1 == '1'){
    alert("Pelaaja heitti ykkösen. Vuorojen vaihto.")
    return window.location.reload(false); 
@@ -20,8 +26,6 @@ function rollDice2(){
 }
 }
 function rollDice(){
-
-
   var die1 = document.getElementById("die1");
   var status = document.getElementById("status")
   var d1 = Math.floor(Math.random() * 6) + 1;
